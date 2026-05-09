acfd0e64d5e4d6e7f7a464693cda9616ba7bcbd2
tshimsthedoor <tshims79@gmail.com>
2026-05-09 15:58:00 +0200

    first commit

---
+++ file_one.py
@@ -0,0 +1,9 @@
+# Define a function that adds two numbers
+def add_numbers(number_one, number_two):
+    return number_one + number_two
+
+# Call the function with two numbers
+result = add_numbers(1, 2)
+
+# Print the result
+print("The sum of the numbers is " + str(result))
